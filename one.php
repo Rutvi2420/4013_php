886e57a86e7b54fe86673e12c065df12763dc065
Rutvi2420 <meghpararutvi@gmail.com>
2025-08-20 12:00:34 +0530

    one

---
+++ one.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 style="color: pink;">Rutvi Meghpara</H1>
+        <?php
+         print  "RUTVI";
+    ?>
+        <br>
+        <hr>
+        <?php
+           print  "<b style='color: green;'> fr" "Meghpara";    
+    ?>
+
+</body>
+</html>
