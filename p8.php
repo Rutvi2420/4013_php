3c9ff48b1c009726ae0963d65e9caae1f0fea5bc
Rutvi2420 <meghpararutvi@gmail.com>
2025-08-29 11:39:48 +0530

    Add files via upload

---
+++ p8.php
@@ -0,0 +1,51 @@
+<html>
+
+<body>
+    <h1>Array Operations</h1>
+    <form action="" method="post">
+        <input type=radio name=choice value=display>Display Array<br />
+        <input type=radio name=choice value=sort>Sorted Array<br />
+        <input type=radio name=choice value=dupli>Without Duplicate<br />
+        <input type=radio name=choice value=pop>Delete Last<br />
+        <input type=radio name=choice value=rev>Array Reverse<br />
+        <input type=radio  value=search>Array Search<br /><br/>
+        <input type=Submit>
+</body>
+
+</html>
+
+<?php
+if ($_POST) {
+    $countries = array("Raj", "parth", "Priyanshi", "Park", "druvi","Raj", "Kush", "visha", "Aasha");
+    $val = $_POST['choice'];
+    switch ($val) {
+        case "display":
+            foreach ($countries as $value)
+                echo "<br>" . $value;
+            break;
+        case "sort":
+            sort($countries);
+            foreach ($countries as $value)
+                echo "<br>" . $value;
+            break;
+        case "dupli":
+            $uarray = array_unique($countries);
+            foreach ($uarray as $value)
+                echo "<br>" . $value;
+            break;
+        case "pop":
+            array_pop($countries);
+            foreach ($countries as $value)
+                echo "<br>" . $value;
+            break;
+        case "rev":
+            $revarr = array_reverse($countries);
+            foreach ($revarr as $value)
+                echo "<br>" . $value;
+            break;
+        case "search":
+            echo "<br/><br/>Position - " . array_search("Italy", $countries, true);
+            break;
+    }
+}
+?>
